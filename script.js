8073d9fd05bab4cd9c480e855632b3100753a67c
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-11 19:26:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(digits) {
+    if (!digits) return []; // if the input is empty or null
+    const mapping = ['0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+    let result = ['']; // array with a single empty string
+
+    for (let i = 0; i < digits.length; i++) {
+        const digit = digits[i];
+        const letters = mapping[digit];
+        const temp = [];
+
+        for (let j = 0; j < letters.length; j++) {
+            for (let k = 0; k < result.length; k++) {
+                temp.push(result[k] + letters[j]);
+            }
+        }
+        result = temp;
+    }
+    return result.sort();
 }
+
 
 module.exports = letterCombinations;
