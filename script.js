ac8ecc41518d056512935925d5bdb9e8b6b50b81
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-31 12:24:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-function letterCombinations(digits) {
-    if (!digits) return []; // if the input is empty or null
-    const mapping = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
-    let result = ['']; // array with a single empty string
-
-    for (let i = 0; i < digits.length; i++) {
-        const digit = digits[i];
-        const letters = mapping[digit];
-        const temp = [];
-
-        for (let j = 0; j < letters.length; j++) {
-            for (let k = 0; k < result.length; k++) {
-                temp.push(result[k] + letters[j]);
-            }
-        }
-        result = temp;
+function letterCombinations(input) {
+    
+  let mapping = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz"
+  };
+  let result = [''];
+  for (let i = 0; i < input.length; i++) {
+    let digit = input[i];
+    let letters = mapping[digit];
+    let newResult = [];
+    for (let j = 0; j < letters.length; j++) {
+      for (let k = 0; k < result.length; k++) {
+        newResult.push(result[k] + letters[j]);
+      }
     }
-    return result.sort();
+    result = newResult;
+  }
+  return result.sort();
 }
+
 
 
 module.exports = letterCombinations;
